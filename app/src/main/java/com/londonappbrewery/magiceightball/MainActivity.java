dbffd031f35b9acfcde86d8771e533d48832bbdc
Ian KIM <magictical@gmail.com>
2018-03-30 16:23:42 +0900

    Finish magic-ball quiz

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,11 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView ballImage = findViewById(R.id.image_eightBall);
+
+        final int[] fortuneBox = new int[] {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+        final Random random = new Random();
+
+
+
+        Button askButton = (Button) findViewById(R.id.askButton);
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int ranNumber = random.nextInt(5);
+                ballImage.setImageResource(fortuneBox[ranNumber]);
+            }
+        });
     }
 }
